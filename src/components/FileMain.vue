f12dd65a7eab65a0ecf86cd2d3f1cfdbf997025e
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-24 17:24:19 +0100

    Problem with loop

--- src/components/FileMain.vue
+++ src/components/FileMain.vue
@@ -1,13 +1,54 @@
 <template>
   <main>
-      <file-card/>
+      <div class="container">
+        <div class="row">
+            <file-card v-for="album in arrAlbums" :key="album.title"/>
+        </div>
+      </div>
   </main>
 </template>
 
 <script>
 import FileCard from './FileCard.vue'
+
 export default {
     name: "FileMain",
+    daya(){
+        return{
+            arrAlbums: [
+                {
+                    poster:	"https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+                    title:	"New Jersey",
+                    genre:	"Rock",
+                    year:	"1988",
+                },
+                {
+                    poster:	"https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+                    title:	"New Jersey",
+                    genre:	"Rock",
+                    year:	"1988",
+                },
+                {
+                    poster:	"https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+                    title:	"New Jersey",
+                    genre:	"Rock",
+                    year:	"1988",
+                },
+                {
+                    poster:	"https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+                    title:	"New Jersey",
+                    genre:	"Rock",
+                    year:	"1988",
+                },
+                {
+                    poster:	"https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+                    title:	"New Jersey",
+                    genre:	"Rock",
+                    year:	"1988",
+                }
+            ]
+        }
+    } ,
     components: { 
         FileCard 
     },
@@ -17,3 +58,4 @@ export default {
 <style lang="scss" scoped>
 
 </style>
+
