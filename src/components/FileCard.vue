3f669ca84d36672ba0caf3749b2b412a03cc82f6
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-24 17:24:33 +0100

    Problem with loop

--- src/components/FileCard.vue
+++ src/components/FileCard.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="album_card">
-    <img src=	"https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-    <h1>Titolo</h1>
-    <h2>Autore</h2>
-    <p>Anno</p>
+  <div class="col-6 col-md-4 col-lg-3">
+    <div class="album_card">
+      <img src=	"https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="" class="img-fluid">
+      <h1>Titolo</h1>
+      <h2>Autore</h2>
+      <p>Anno</p>
+    </div>
   </div>
 </template>
 
