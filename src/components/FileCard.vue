ef2c425ff603cf1b22668298a5ee4d4882edc9c3
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-25 08:57:41 +0100

    Add card style

--- src/components/FileCard.vue
+++ src/components/FileCard.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="col">
+  <div class="col mb-5">
     <div class="album_card p-3 text-center">
-      <img :src="albumData.poster" :alt="albumData.title" class="img-fluid">
-      <h1 class="text-uppercase fw-bold text-light">{{ albumData.title }}</h1>
-      <h2>{{ albumData.genre }}</h2>
+      <div class="img_wrapper mb-3">
+        <img :src="albumData.poster" :alt="albumData.title" >
+      </div>
+      <p class="text-uppercase text-light fw-bold fs-4">{{ albumData.title }}</p>
+      <p class="fs-5 mb-0">{{ albumData.genre }}</p>
       <p>{{ albumData.year }}</p>
     </div>
   </div>
@@ -21,6 +23,16 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/styles/partials/variables.scss";
 .album_card{
+  height: 380px;
   background-color: $blue_card;
+  color: $text_color;
+  .img_wrapper{
+    width: 160px;
+    height: 160px;
+    img{
+      width: 100%;
+      object-fit: cover;
+    }
+  }
 }
 </style>
